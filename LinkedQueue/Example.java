bfc38036af3486729475497758e43783cc451d5e
Abdo Kamel <69877498+abdokamel2001@users.noreply.github.com>
2022-12-08 00:02:16 +0200

    Update Example.java

--- LinkedQueue/Example.java
+++ LinkedQueue/Example.java
@@ -1,1 +1,15 @@
-
+public class LinkedQueueEx {
+    public static void main(String[] args) {
+        LinkedQueue<Integer> q1 = new LinkedQueue<>(1,3,6,7,9);
+        q1.dequeue();
+        q1.dequeue();
+        System.out.println(q1);
+        q1.enqueue(5);
+        System.out.println(q1 + " , size = " + q1.getSize());
+        System.out.println("Removing : " + q1.dequeue());
+        System.out.println("Removing : " + q1.dequeue());
+        System.out.println("Removing : " + q1.dequeue());
+        System.out.println("Removing : " + q1.dequeue());
+        System.out.println("Removing : " + q1.dequeue());
+    }
+}
