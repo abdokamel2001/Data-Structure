9f2e8eaf0db9af7e2c992aef32e705e16ff64d5e
Abdo Kamel <69877498+abdokamel2001@users.noreply.github.com>
2022-12-08 00:03:51 +0200

    Update Example.java

--- LinkedStack/Example.java
+++ LinkedStack/Example.java
@@ -1,1 +1,14 @@
-
+public class StackEx {
+    public static void main(String[] args) {
+        Stack<Boolean> s1 = new Stack<>(5);
+        s1.push(Boolean.TRUE);
+        s1.push(false);
+        s1.push(Boolean.FALSE);
+        s1.push(true);
+        System.out.println(s1);
+        System.out.println("Removing : " + s1.pop());
+        System.out.println("Removing : " + s1.pop());
+        System.out.println("Removing : " + s1.pop());
+        System.out.println("Removing : " + s1.pop());
+    }
+}
