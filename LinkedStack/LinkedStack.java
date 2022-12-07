9ecff869e4f2efdd4102be39d0c8b9fc8ad3bda9
Abdo Kamel <69877498+abdokamel2001@users.noreply.github.com>
2022-12-08 00:03:29 +0200

    Update LinkedStack.java

--- LinkedStack/LinkedStack.java
+++ LinkedStack/LinkedStack.java
@@ -1,1 +1,39 @@
+public class LinkedStack <T> {
 
+    private final LinkedList<T> list = new LinkedList<>();
+
+    public LinkedStack(){}
+
+    @SafeVarargs
+    public LinkedStack(T... items){
+        for(T x: items) list.addFirst(x);
+    }
+
+    public int getSize(){return list.getSize();}
+
+    public boolean isEmpty(){return list.getSize() == 0;}
+
+    public T peek(){
+        if(isEmpty()) {emptyError(); return null;}
+        return list.getFirst();
+    }
+
+    public T pop(){
+        if(isEmpty()) {emptyError(); return null;}
+        T item = list.getFirst();
+        list.removeFirst();
+        return item;
+    }
+
+    public void push(T item){list.addFirst(item);}
+
+    public LinkedList<T> getStack(){return list;}
+
+    public String toString(){return list.toString();}
+
+    public <E> E[] toArray(E[] a) { return list.toArray(a);}
+
+    private void emptyError() {
+        System.out.println("The queue is empty");
+    }
+}
