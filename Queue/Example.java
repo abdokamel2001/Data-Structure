ea007389d32135d0aac8bc2ddf0ad4a905833be7
Abdo Kamel <69877498+abdokamel2001@users.noreply.github.com>
2022-12-09 00:55:25 +0200

    Update Example.java

--- Queue/Example.java
+++ Queue/Example.java
@@ -1,1 +1,14 @@
-
+public class Example {
+    public static void main(String[] args) {
+        Queue<String> q1 = new Queue<>(3);
+        q1.enqueue("abdo");
+        q1.enqueue("ahmed");
+        q1.enqueue("kamel");
+        q1.enqueue("apple");
+        System.out.println(q1);
+        System.out.println("Removing : " + q1.dequeue());
+        System.out.println("Removing : " + q1.dequeue());
+        System.out.println("Removing : " + q1.dequeue());
+        System.out.println("Removing : " + q1.dequeue());
+    }
+}
